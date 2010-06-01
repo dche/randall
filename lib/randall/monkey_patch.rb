f0f410880518867d4affa1a4108170dc7113c469
Diego Che <chekenan@gmail.com>
2010-06-02 04:03:42 +0800

    method documents.

--- lib/randall/monkey_patch.rb
+++ lib/randall/monkey_patch.rb
@@ -1,21 +1,27 @@
 # encoding: utf-8
 #
-# 
+# copyright (c) 2010, Diego Che
+#
 
 module RandallMonkey
+  
   module PickableArray
+    # Randomly pick an element from the receiver.
     def pick
       self[Kernel.rand(self.size)]
     end
   end
 
   module RandableRegexp
+    # Generate a String that matches the receiver.
     def rand
       @randall ||= Randall.new(String, :like => self)
       @randall.next
     end
   end
   
+  # Enable the monkey patch.
+  # After this message, you have +Array#pick+ and +Regexp#rand+.
   def self.patch
     Array.class_eval do
       include PickableArray
